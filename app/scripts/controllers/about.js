8e69446603b460f152e98a5073fd5760e043675a
Shawn Rider <shawn@shawnrider.com>
2015-07-28 20:17:25 -0700

    Initial commit.
    Change-Id: Ifb366289cdb493fc07d562fbc5fbc542bcf7b521

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bdayApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bdayApp
+ */
+angular.module('bdayApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bdayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bdayApp
+ */
+angular.module('bdayApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
